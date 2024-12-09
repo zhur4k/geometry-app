e5ce073ca1e15b27f8e89d43eea8f8d43ced1215
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-09 16:56:32 +0300

    feat:test working.

--- src/main/java/com/geometry/Main.java
+++ src/main/java/com/geometry/Main.java
@@ -2,6 +2,20 @@ package com.geometry;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Shape circle = new Circle(5);
+        System.out.println("Круг:");
+        System.out.println("Площадь: " + circle.getSquare());
+        System.out.println("Периметр: " + circle.getPerimeter());
+
+        Shape rectangle = new Rectangle(4, 7);
+        System.out.println("Прямоугольник:");
+        System.out.println("Площадь: " + rectangle.getSquare());
+        System.out.println("Периметр: " + rectangle.getPerimeter());
+
+        Shape triangle = new Triangle(6);
+        System.out.println("Круг:");
+        System.out.println("Площадь: " + triangle.getSquare());
+        System.out.println("Периметр: " + triangle.getPerimeter());
     }
 }
