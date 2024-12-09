51e2f4fbb76d6cf3921099e4b38add4dd14db51a
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-09 17:19:11 +0300

    feat:add using new functional.

--- src/main/java/com/geometry/Main.java
+++ src/main/java/com/geometry/Main.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
 
         Shape circle = new Circle(5);
-        System.out.println("Круг:");
+        System.out.println(circle.getType());
         System.out.println("Площадь: " + circle.getSquare());
         System.out.println("Периметр: " + circle.getPerimeter());
 
         Shape rectangle = new Rectangle(4, 7);
-        System.out.println("Прямоугольник:");
+        System.out.println(rectangle.getType());
         System.out.println("Площадь: " + rectangle.getSquare());
         System.out.println("Периметр: " + rectangle.getPerimeter());
 
         Shape triangle = new Triangle(6);
-        System.out.println("Круг:");
+        System.out.println(triangle.getType());
         System.out.println("Площадь: " + triangle.getSquare());
         System.out.println("Периметр: " + triangle.getPerimeter());
     }
